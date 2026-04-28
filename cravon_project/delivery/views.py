f6019d1b2f3af2e3a365918fb76f23e4d7ad628f
Aditi Kushwaha <137718055+AditiKushwaha01@users.noreply.github.com>
2026-04-28 17:43:16 +0530

    added sign in sign up page

--- cravon_project/delivery/views.py
+++ cravon_project/delivery/views.py
@@ -6,3 +6,20 @@ def say_hello(request):
     # to connect frontend and backend below render is used
     # in one line frontend page is connected
     return render(request, "index.html")
+
+def open_signup(request):
+    return render(request, "signup.html")
+
+def open_signin(request):
+    return render(request, "signin.html")
+
+def signup(request): #if the entered details are correct or not
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        address = request.POST.get("address")
+        return HttpResponse(f"Username: {username}, Password: {password}, Email: {email}, Mobile: {mobile}, Address: {address}")
+    else:
+        return HttpResponse("Invalid response!")
